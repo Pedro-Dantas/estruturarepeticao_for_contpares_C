e0fe99555f6cb08fbde54db4ad095b79ee24e611
Pedro Dantas <dev.pedrodantas@gmail.com>
2017-03-14 15:50:33 -0300

    Codigo para saber quantos numeros pares existem entre dois numeros

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Estudo de estrutura repetição em C, contadores de pares no intervalo entre 150 e 200.
+
+int main(int argc, char *argv[]) {
+	
+	int i;
+	int cont = 0;
+
+	for(i = 150; i <= 200; i = i + 2){
+		cont++;
+		
+	
+		
+	}
+	
+	printf("\n Existem %d numeros pares no intervalo de 150 a 200\n", cont);
+	
+	
+	return 0;	
+}
+
